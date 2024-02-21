e7221e211c54c29747e2eecba7538ea72f86bd26
VivianBuelvas <avivian@uninorte.edu.co>
2024-02-21 15:40:49 -0500

    Hola, mundo

---
+++ JavaApplication118/src/javaapplication118/JavaApplication118.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication118;
+
+/**
+ *
+ * @author pc
+ */
+public class JavaApplication118 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo");
+                
+    }
+    
+}
